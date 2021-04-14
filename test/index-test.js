a2ed67a20384c985ac1427bc191d46f00d9dcf68
bailey watkins <bailey.watkins@rocketmail.com>
2021-04-14 19:46:14 -0400

    completed assignment.

--- test/index-test.js
+++ test/index-test.js
@@ -1,53 +1,53 @@
-const expect = require('expect')
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+//const expect = require('expect')
+//const fs = require('fs')
+//const jsdom = require('mocha-jsdom')
+//const path = require('path')
 
 
-describe('index', () => {
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
+//describe('index', () => {
+  //jsdom({
+    //src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+  //})
 
-  describe('receivesAFunction(callback)', () => {
-    it('receives a function and calls it', () => {
-      const spy = expect.createSpy()
+  //describe('receivesAFunction(callback)', () => {
+    //it('receives a function and calls it', () => {
+      //const spy = expect.createSpy()
 
-      receivesAFunction(spy)
+      //receivesAFunction(spy)
 
-      expect(spy).toHaveBeenCalled()
-    })
-  })
+      //expect(spy).toHaveBeenCalled()
+    //})
+  //})
 
-  describe('returnsANamedFunction()', () => {
-    var fn
+  //describe('returnsANamedFunction()', () => {
+    //var fn
 
-    before(() => {
-      fn = returnsANamedFunction()
-    })
+    //before(() => {
+     // fn = returnsANamedFunction()
+   // })
 
-    it('returns a function', () => {
-      expect(fn).toBeA('function')
-    })
+    //it('returns a function', () => {
+     // expect(fn).toBeA('function')
+    //})
 
-    it('returns a named function', () => {
-      expect(fn.name).toNotEqual('')
-    })
-  })
+    //it('returns a named function', () => {
+      //expect(fn.name).toNotEqual('')
+   // })
+ // })
 
-  describe('returnsAnAnonymousFunction()', () => {
-    var fn
+  //describe('returnsAnAnonymousFunction()', () => {
+    //var fn
 
-    before(() => {
-      fn = returnsAnAnonymousFunction()
-    })
+    //before(() => {
+      //fn = returnsAnAnonymousFunction()
+    //})
 
-    it('returns a function', () => {
-      expect(fn).toBeA('function')
-    })
+    //it('returns a function', () => {
+      //expect(fn).toBeA('function')
+    //})
 
-    it('returns an anonymous function', () => {
-      expect(fn.name).toEqual('')
-    })
-  })
-})
+    //it('returns an anonymous function', () => {
+      //expect(fn.name).toEqual('')
+    //})
+  //})
+//})
